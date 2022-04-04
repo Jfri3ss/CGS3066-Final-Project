8a06de4401ed59293f76b25bc6dabc24ece96f0e
Steve Jobs <77308708+Jfri3ss@users.noreply.github.com>
2022-04-04 17:51:01 -0400

    Add files via upload

---
+++ sql/cgsfinal.sql
@@ -0,0 +1,23 @@
+USE CGS;
+CREATE TABLE menu (dish VARCHAR(35), price INT);
+SHOW TABLES;
+INSERT INTO menu (dish,price)
+VALUES("C++ Carbonara",30);
+SELECT * FROM menu;
+INSERT INTO menu (dish,price)
+VALUES("Java Jackfruit Taco", 20);
+SELECT * FROM menu;
+INSERT INTO menu (dish,price)
+VALUES("Python Panini", 16);
+INSERT INTO menu (dish,price)
+VALUES("C# Cauliflower Wings", 10);
+INSERT INTO menu (dish,price)
+VALUES("HTML Ham Hoagie", 18);
+INSERT INTO menu (dish,price)
+VALUES("SQL Swedish Meatballs", 25);
+SELECT * FROM menu;
+INSERT INTO menu (dish,price)
+VALUES("Rust Ravioli", 30);
+INSERT INTO menu (dish,price)
+VALUES("PHP Pesto Pizza", 17);
+SELECT * FROM menu;
