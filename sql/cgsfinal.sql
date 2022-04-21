f8536db50dc3e9b72c82fc1f698c6fa567f828cf
Steve Jobs <77308708+Jfri3ss@users.noreply.github.com>
2022-04-21 11:52:17 -0400

    Update cgsfinal.sql

--- sql/cgsfinal.sql
+++ sql/cgsfinal.sql
@@ -1,23 +1,24 @@
-USE CGS;
-CREATE TABLE menu (dish VARCHAR(35), price INT);
+CREATE DATABASE db;
+USE db;
+CREATE TABLE menu (dish VARCHAR(35), price INT, vegetarian VARCHAR(5));
 SHOW TABLES;
-INSERT INTO menu (dish,price)
-VALUES("C++ Carbonara",30);
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("C++ Carbonara", 30, "Yes");
 SELECT * FROM menu;
-INSERT INTO menu (dish,price)
-VALUES("Java Jackfruit Taco", 20);
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("Java Jackfruit Taco", 20, "Yes");
 SELECT * FROM menu;
-INSERT INTO menu (dish,price)
-VALUES("Python Panini", 16);
-INSERT INTO menu (dish,price)
-VALUES("C# Cauliflower Wings", 10);
-INSERT INTO menu (dish,price)
-VALUES("HTML Ham Hoagie", 18);
-INSERT INTO menu (dish,price)
-VALUES("SQL Swedish Meatballs", 25);
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("Python Panini", 16, "No");
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("C# Cauliflower Wings", 10, "Yes");
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("HTML Ham Hoagie", 18, "No");
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("SQL Swedish Meatballs", 25, "No");
 SELECT * FROM menu;
-INSERT INTO menu (dish,price)
-VALUES("Rust Ravioli", 30);
-INSERT INTO menu (dish,price)
-VALUES("PHP Pesto Pizza", 17);
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("Rust Ravioli", 30, "No");
+INSERT INTO menu (dish,price,vegetarian)
+VALUES("PHP Pesto Pizza", 17, "Yes");
 SELECT * FROM menu;
