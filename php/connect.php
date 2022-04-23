964c8a8e034e3d41b5de6ef82b969dfc6e300d90
Steve Jobs <77308708+Jfri3ss@users.noreply.github.com>
2022-04-22 23:07:57 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -4,7 +4,7 @@ $price = $_POST['price'];
 $vegetarian = $_POST['vegetarian'];
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli('Steves-iMac.local', 'user1', 'password12', 'db');
+$mysqli = new mysqli('Jeremiahs-MacBook-Pro.local', 'user1', 'password12', 'db');
 
 $stmt = $mysqli->prepare("INSERT INTO menu VALUES (?, ?, ?)");
 $stmt->bind_param('sis', $entreeName, $price, $vegetarian);
